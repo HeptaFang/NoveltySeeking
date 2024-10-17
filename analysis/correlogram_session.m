294748910ee798bb96f6b37fc2af8ca30135c8b9
HeptaFang <septafang@gmail.com>
2024-10-17 12:58:12 -0500

    correlogram and a nice timer tool.

--- analysis/correlogram_session.m
+++ analysis/correlogram_session.m
@@ -59,9 +59,17 @@ for i = 1:length(dataset_names)
     all_data{i}.trial_len = trial_len;
 end
 
-cell_area = session_cell_area;
-cell_id = session_cell_id;
-N = session_N;
+% check if pre and post session has the same cell id except thalamus
+thalamus_idx_pre = strcmp(session_cell_area_pre, 'Thalamus');
+thalamus_idx_post = strcmp(session_cell_area_post, 'Thalamus');
+assert(isequal(session_cell_area_pre(~thalamus_idx_pre), ...
+    session_cell_area_post(~thalamus_idx_post)), 'cell_area not equal');
+assert(isequal(session_cell_id_pre(~thalamus_idx_pre), ...
+    session_cell_id_post(~thalamus_idx_post)), 'cell_id not equal');
+
+cell_area = session_cell_area_pre;
+cell_id = session_cell_id_pre;
+N = session_N_pre;
 
 % parameters
 temp_range = 200; % ms
@@ -70,18 +78,31 @@ N_shuffle = 100;
 smooth_kernel = ones(1, 5) / 5; % rectangular kernel
 % smooth_kernel = gausswin(5); % gaussian kernel
 
+total_figure_num = N*(N-1)/2;
+plot_count = 0;
+total_time_spent = 0;
+
 % plotting for each neuron pair
-% fix cell index!!
 for i = 1:(N-1)
     for j = (i+1):N
+        plot_count = plot_count + 1;
+        fprintf("Plot %d/%d\n", plot_count, total_figure_num);
+        tic;
         % set up subplot 3x4
-        figure("Visible", "off", "Position", [0, 0, 1600, 1200]);
+        figure("Visible", "off", "Position", [0, 0, 3200, 2400]);
         sgtitle([cell_area{i}, ' ', cell_id{i}, ' vs ', cell_area{j}, ' ', cell_id{j}]);
         tiles = tiledlayout(3, 4);
+
+        global_max_y = 0;
 
         % loop over all sessions
         for dataset_name_idx = 1:length(dataset_names)
             nexttile;
+
+            if (strcmp(cell_area{i}, 'Thalamus') || strcmp(cell_area{j}, 'Thalamus')) &&...
+                strcmp(states{dataset_name_idx}(1:4), 'Post')
+                continue;
+            end
 
             spikes = all_data{dataset_name_idx}.spikes;
             n_trial = all_data{dataset_name_idx}.n_trial;
@@ -156,15 +177,22 @@ for i = 1:(N-1)
             std_count_shuffled = std(count_over_chance_shuffled, 1);
             max_count_shuffled = max(count_over_chance_shuffled, [], 1);
             min_count_shuffled = min(count_over_chance_shuffled, [], 1);
-            
 
-            max_y = 1.1*max(max(count_over_chance), 1);
+            % log scale
+            count_over_chance = log(count_over_chance);
+            smoothed_count = log(smoothed_count);
+            mean_count_shuffled = log(mean_count_shuffled);
+            std_count_shuffled = std_count_shuffled/mean_count_shuffled;
+
+            max_y = 1.1*max(max(abs(count_over_chance(~isinf(count_over_chance)))), 1);
+            global_max_y = max(global_max_y, max_y);
+
             % plot correlogram
             hold on;
             % error bar area and mean
             fill([bin_centers, fliplr(bin_centers)], ...
                 [mean_count_shuffled + std_count_shuffled, fliplr(mean_count_shuffled - std_count_shuffled)], ...
-                [0, 0, 0.8], 'EdgeColor', 'none', 'FaceAlpha', 0.7);
+                [0.8, 0.8, 0.8], 'EdgeColor', 'none', 'FaceAlpha', 0.7);
             % fill([bin_centers, fliplr(bin_centers)], ...
             %     [max_count_shuffled, fliplr(min_count_shuffled)], ...
             %     [0.8, 0.8, 0.8], 'EdgeColor', 'none', 'FaceAlpha', 0.7);
@@ -174,29 +202,52 @@ for i = 1:(N-1)
             plot(bin_centers, count_over_chance, 'r.-', 'LineWidth', 1.5, 'Color', [1, 0, 0, 0.5]);
             plot(bin_centers, smoothed_count, 'b-', 'LineWidth', 1.5);
             
-            plot([-temp_range, temp_range], [1, 1], 'k--', 'LineWidth', 1);
-            plot([0, 0], [0, max_y], 'k--', 'LineWidth', 1);
+            plot([-temp_range, temp_range], [0, 0], 'k--', 'LineWidth', 1);
             hold off;
+            
+            % replace underscore with space
+            data_title = strrep(states{dataset_name_idx}, '_', ' ');
 
-            title(dataset_name);
+            title(data_title, 'FontSize', 18);
             xlabel('T_1 - T_2 (ms)');
-            ylabel('Count over chance level');
+            ylabel('log correlation');
             xlim([-temp_range, temp_range]);
-            ylim([0, max_y]);
+        end
+
+        % make all ylim the same
+        for k = 1:12
+            nexttile(k);
+            ylim([-global_max_y, global_max_y]);
+            hold on;
+            plot([0, 0], [-global_max_y, global_max_y], 'k--', 'LineWidth', 1);
+            hold off;
             legend('Shuffled range', 'Shuffled mean', 'Data', 'Smoothed data');
         end
 
-    % save figure
-    fig_folder = ['../figures/correlograms/', session_type, '_', int2str(session)];
-    if ~exist(fig_folder, 'dir')
-        mkdir(fig_folder);
-    end
-    fig_name = [fig_folder, '/correlogram_', cell_area{i}, '_', cell_id{i}, ...
-        '_vs_', cell_area{j}, '_', cell_id{j}, '.png'];
-    fprintf("Save %s\n", fig_name);
-    saveas(gcf, fig_name);
+        % global title
+        sgtitle([cell_area{i}, ' ', cell_id{i}, ' vs ', cell_area{j}, ' ', cell_id{j}]);
 
-    close all;
+        % save figure
+        fig_folder = ['../figures/correlograms/', session_type, '_', int2str(session_idx)];
+        if ~exist(fig_folder, 'dir')
+            mkdir(fig_folder);
+        end
+        fig_name = [fig_folder, '/correlogram_', cell_area{i}, '_', cell_id{i}, ...
+            '_vs_', cell_area{j}, '_', cell_id{j}, '.png'];
+        fprintf("(%d/%d)Save %s\n", plot_count, total_figure_num, fig_name);
+        saveas(gcf, fig_name);
+
+        close all;
+        time_spent = toc;
+        total_time_spent = total_time_spent + time_spent;
+        eta = (total_figure_num - plot_count)*total_time_spent/plot_count;
+        fprintf("Time spent: %f, total: %02d:%02d:%02d, eta: %02d:%02d:%02d\n\n", time_spent, ...
+            floor((total_time_spent/60/60)), ...
+            floor(mod(total_time_spent/60, 60)), ...
+            floor(mod(total_time_spent, 60)), ...
+            floor((eta/60/60)), ...
+            floor(mod(eta/60, 60)), ...
+            floor(mod(eta, 60)));
     end
 end
 
